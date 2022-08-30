17dca305aede0e1320eb5a1a8877c74035a55f2d
emcdona1 <emcdona1@neiu.edu>
2022-08-29 21:38:59 -0500

    Add arguments to each version

--- anova_feature_selection.py
+++ anova_feature_selection.py
@@ -8,8 +8,8 @@ from utilities import load_spectral_data
 
 
 # feature selection
-def reduce_features(num_features='all'):
-    X, y = load_spectral_data('subgenus', True)
+def reduce_features(level='subgenus', num_features='all'):
+    X, y = load_spectral_data(level, True)
     # split into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33,
                                                         random_state=1, shuffle=True, stratify=y)
@@ -21,24 +21,26 @@ def reduce_features(num_features='all'):
     X_train_fs = fs.transform(X_train)  # note: this doesn't do anything because k='all'
     # transform test input data
     X_test_fs = fs.transform(X_test)  # note: this doesn't do anything because k='all'
-    for i in range(len(fs.scores_)):
-        print(f'Feature {i}: {fs.scores_[i]}')
+    # for i in range(len(fs.scores_)):
+    #     print(f'Feature {i}: {fs.scores_[i]}')
     # plot the scores
-    plt.plot([int(i) for i in fs.feature_names_in_], fs.scores_, 'ro')
-    plt.show()
-    return X_train_fs, X_test_fs, fs
+    # plt.plot([int(i) for i in fs.feature_names_in_], fs.scores_)
+    # plt.title(level.capitalize())
+    # plt.xlabel('nanometers')
+    # plt.ylabel('ANOVA/f-score')
+    # plt.show()
+    return (X_train_fs, y_train), (X_test_fs, y_test), fs
 
 
-def plot_feature_significance():
+def plot_feature_significance(level='subgenus'):
     plt.clf()
-    level = 'subsection'
     X, y = load_spectral_data(level, True)
     fs = SelectKBest(score_func=f_classif, k='all')
     fs.fit(X, y)
     # plot the scores
     plt.plot([int(i) for i in fs.feature_names_in_], fs.scores_)
     plt.title(level.capitalize())
-    plt.xlabel('nm')
+    plt.xlabel('nanometers')
     plt.ylabel('ANOVA/f-score')
     plt.savefig(f'{level}.png')
     plt.show()
@@ -46,18 +48,17 @@ def plot_feature_significance():
     #     print(f'Feature {i}: {fs.scores_[i]}')
 
 
-def five_fold_validation_of_feature_significance():
+def five_fold_validation_of_feature_significance(level='subgenus', n_folds=5):
     plt.clf()
-    level = 'species'
     X, y = load_spectral_data(level, True)
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
+    skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=1)
     for train_idx, test_idx in skf.split(X, y):
         X_train, y_train = X.iloc[train_idx], y.iloc[train_idx]
         fs = SelectKBest(score_func=f_classif, k='all')
         fs.fit(X_train, y_train)
         plt.plot([int(i) for i in fs.feature_names_in_], fs.scores_)
     plt.title(level.capitalize())
-    plt.xlabel('nm')
+    plt.xlabel('nanometers')
     plt.ylabel('ANOVA/f-score')
     plt.savefig(f'{level}-5_fold.png')
     plt.show()
