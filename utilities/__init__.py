ba7d48d4521705c2582f9b6b15d0d55acbf54472
emcdona1 <emcdona1@neiu.edu>
2022-09-01 15:54:14 -0500

    Add package init

---
+++ utilities/__init__.py
@@ -0,0 +1,3 @@
+from utilities.utilities import load_spectral_data, display_results
+from utilities.feature_reductions import pca, anova_reduce_features
+from utilities.classifications import cart, knn, naive_bayes
