4c75ea7852d1d2e96f2cb61d171676d813ed8d6b
emcdona1 <emcdona1@neiu.edu>
2022-09-21 14:18:01 -0500

    Remove test/train split for clarity (do it earlier)

--- utilities/feature_reductions.py
+++ utilities/feature_reductions.py
@@ -3,21 +3,16 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
-from sklearn.model_selection import train_test_split
 
 
-def pca(X_train, y_train, X_test=None, y_test=None, n_features=10) \
+def pca(X_train, y_train, X_test, y_test, n_features=10) \
         -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray, PCA):
-    if X_test.shape[0] != 0:
-        X_train, X_test, y_train, y_test = train_test_split(X_train, y_train,
-                                                            test_size=0.2, random_state=1,
-                                                            shuffle=True, stratify=y_train)
     p = PCA(n_components=n_features)
     p.fit(X_train)
     X_transformed_train = p.transform(X_train)
     X_transformed_test = p.transform(X_test)
     # _view_pca(X_transformed, y, level)
-    return X_transformed_train, y_train, X_transformed_test, y_test  #, p
+    return X_transformed_train, y_train, X_transformed_test, y_test
 
 
 def _view_pca(X_transformed, labels, level):
@@ -36,12 +31,8 @@ def _view_pca(X_transformed, labels, level):
     plt.show()
 
 
-def anova(X_train, y_train, X_test=None, y_test=None, n_features='all') \
-        -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray, SelectKBest):
-    if X_test.shape[0] != 0:
-        X_train, X_test, y_train, y_test = train_test_split(X_train, y_train,
-                                                            test_size=0.2, random_state=1,
-                                                            shuffle=True, stratify=y_train)
+def anova(X_train, y_train, X_test, y_test, n_features='all') -> \
+        (np.ndarray, np.ndarray, np.ndarray, np.ndarray, SelectKBest):
     fs = SelectKBest(score_func=f_classif, k=n_features)
     fs.fit(X_train, y_train)
     X_train_fs = fs.transform(X_train)  # note: this doesn't do anything when k='all'
@@ -60,12 +51,8 @@ def _view_feature_usefulness(fs):
     plt.show()
 
 
-def mutual_info(X_train, y_train, X_test=None, y_test=None, n_features='all') \
+def mutual_info(X_train, y_train, X_test, y_test, n_features='all') \
         -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray, SelectKBest):
-    if X_test.shape[0] != 0:
-        X_train, X_test, y_train, y_test = train_test_split(X_train, y_train,
-                                                            test_size=0.2, random_state=1,
-                                                            shuffle=True, stratify=y_train)
     kbest = SelectKBest(score_func=mutual_info_classif, k=n_features)
     kbest.fit(X_train, y_train)
     X_train_fs = kbest.transform(X_train)  # note: this doesn't do anything when k='all'
